d81d76eeaea66786a52a359d28ab10f260a07abd
DESKTOP-H9IG1C6\Nastya <anastacy2882@gmail.com>
2017-11-19 23:49:26 +0200

    all project

---
+++ php/mail.php
@@ -0,0 +1,19 @@
+<?php
+
+$recepient = "anastacy2882@gmail.com";
+$sitename = "Postoyaly-dvor";
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ( $method === 'POST' ) {
+	foreach ( $_POST as $key => $value ) {
+			if ( $value != "" ) {
+				$message .= "" . $key . ":" . " " . $value . "\n";
+			}
+		}
+}
+
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
