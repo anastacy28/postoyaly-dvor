7d17049b65d1b32a1190fab9a6a817b3496397f5
DESKTOP-H9IG1C6\Nastya <anastacy2882@gmail.com>
2017-11-24 20:38:46 +0200

    no message

---
+++ js/double-slider.js
@@ -0,0 +1,46 @@
+ <script>
+      $('.sl-show1').slick({
+        autoplay: true,
+        autoSpeed: 1000,
+  cssEase: 'ease-in',
+  asNavFor: '.sl-nav',
+  fade: true
+});
+$('.sl-nav1').slick({
+  autoplay: true,
+  autoSpeed: 1000,
+  dots: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: '.sl-show',
+  focusOnSelect: true,
+});
+    </script>
+     <script>
+$('.sl-show2').slick({
+  cssEase: 'ease-in',
+  asNavFor: '.sl-nav',
+  fade: true
+});
+$('.sl-nav2').slick({
+  dots: false,
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  asNavFor: '.sl-show',
+  focusOnSelect: true,
+});
+    </script>
+     <script>
+      $('.sl-show3').slick({
+  cssEase: 'ease-in',
+  asNavFor: '.sl-nav',
+  fade: true
+});
+$('.sl-nav3').slick({
+  dots: false,
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  asNavFor: '.sl-show',
+  focusOnSelect: true,
+});
+    </script>
